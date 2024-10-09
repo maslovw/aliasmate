5bcf3458dd92c2de266f548ea235802f67d231d8
Slava Maslov <viacheslav.maslov@esrlabs.com>
2024-10-09 20:29:44 +0200

    add `--` as separator for aliasmate and application arguments
    add executing of the application process in the same process

--- src/main.py
+++ src/main.py
@@ -3,19 +3,29 @@
 import argparse
 import json
 import sys
+import os
+
 try:
     import yaml
 except ImportError:
     yaml = None
 
 def main():
+    # Split sys.argv manually to handle '--'
+    if '--' in sys.argv:
+        idx = sys.argv.index('--')
+        own_args = sys.argv[1:idx]
+        sub_args = sys.argv[idx+1:]
+    else:
+        own_args = sys.argv[1:]
+        sub_args = []
+
+    # Parse own_args
     parser = argparse.ArgumentParser(description="Aliasmate: Command-line alias substitution tool")
     parser.add_argument('-c', '--config', help='Config file (JSON or YAML)', required=True)
-    parser.add_argument('args', nargs=argparse.REMAINDER, help='Arguments to process')
-    args = parser.parse_args()
+    args = parser.parse_args(own_args)
 
     config_file = args.config
-    input_args = args.args
 
     try:
         with open(config_file, 'r') as f:
@@ -63,12 +73,13 @@ def main():
                 position += 1
         return output_tokens
 
-    tokens = input_args
+    tokens = sub_args
     output_tokens = substitute_tokens(tokens, alias_dict)
     application_tokens = application_str.split()
     final_tokens = application_tokens + output_tokens
     command_str = ' '.join(final_tokens)
     print(command_str)
+    os.execvp(final_tokens[0], final_tokens)
 
 if __name__ == '__main__':
     main()
