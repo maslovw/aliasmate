4d9383d6d5a7c6c88782c3303414c303934d9f38
Slava Maslov <viacheslav.maslov@esrlabs.com>
2024-10-11 10:34:32 +0200

    v0.1.3 add check for control and redirection operators
    If special character is detected in argument substitution and it's not
    the very last argument, aliasmate will throw an ERROR message

--- aliasmate/__main__.py
+++ aliasmate/__main__.py
@@ -2,9 +2,9 @@ import argparse
 import json
 import sys
 import subprocess
-from pprint import pprint
+import shlex
 
-VERSION='0.1.2'
+VERSION='0.1.3'
 
 try:
     import yaml
@@ -42,6 +42,11 @@ def split_arguments(argv):
         own_args = argv[1:]
         sub_args = []
     return own_args, sub_args
+
+special_chars = {'|', '<', '>', '&', ';'}
+
+def contains_special_char(s):
+    return not set(s).isdisjoint(special_chars)
 
 def main():
     own_args, sub_args = split_arguments(sys.argv)
@@ -94,8 +99,9 @@ It is possible to use second `--` group of arguments to pass back to aliasmate''
     def substitute_tokens(tokens, alias_dict):
         position = 0
         output_tokens = []
+        special_chars_found_wrong_placed = (False, None)
         if not alias_dict:
-            return tokens
+            return tokens, special_chars_found_wrong_placed
         max_key_length = max(len(key.split()) for key in alias_dict.keys())
         while position < len(tokens):
             match_found = False
@@ -105,21 +111,35 @@ It is possible to use second `--` group of arguments to pass back to aliasmate''
                 seq = tokens[position:position+length]
                 seq_str = ' '.join(seq)
                 if seq_str in alias_dict:
-                    substitution = alias_dict[seq_str].split()
+                    subs_val = alias_dict[seq_str]
+                    substitution = subs_val.split()
                     output_tokens.extend(substitution)
                     position += length
+                    # check if substition contains flow control characters and it's not the last argument
+                    if position < len(tokens):
+                        if contains_special_char(subs_val):
+                            special_chars_found_wrong_placed=(True, subs_val)
                     match_found = True
                     break
             if not match_found:
                 output_tokens.append(tokens[position])
                 position += 1
-        return output_tokens
+        return output_tokens, special_chars_found_wrong_placed
 
     tokens = sub_args
-    output_tokens = substitute_tokens(tokens, alias_dict)
+    output_tokens, wrong_placed_special_character = substitute_tokens(tokens, alias_dict)
+        #raise RuntimeError(f"Found special flow handling in the middle of command: {subs_val}")
     application_tokens = application_str.split()
     final_tokens = application_tokens + output_tokens
     command_str = ' '.join(final_tokens)
+
+    if wrong_placed_special_character[0]:
+        eprint(f"ERROR: Found special flow handling in the middle of command: {wrong_placed_special_character[1]}")
+        eprint(f"ERROR: result command: {command_str}")
+        sys.exit(1)
+
+    # Check for special characters not at the end
+    #check_special_characters(command_str)
 
 
     if is_verbose:
@@ -129,7 +149,10 @@ It is possible to use second `--` group of arguments to pass back to aliasmate''
         sys.exit(0)
 
     try:
-        output = subprocess.run(command_str, shell=True, check=True)
+        subprocess.run(command_str, shell=True, check=True)
+        # execvp will change current alliasmate process with executed command
+        # resulting in the same PID on unix systems
+        # but piping used in the config file won't be possible
         #os.execvp(final_tokens[0], final_tokens)
     except subprocess.CalledProcessError as e:
         sys.exit(e.returncode)
